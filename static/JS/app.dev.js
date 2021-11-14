5808e81e67b50ab13c7c8d1205d433e1ae59ca7c
chrxw <chrwan.ja@gmail.com>
2021-11-15 04:12:58 +0700

    Home finish

---
+++ static/JS/app.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+$("#email").on("input", function () {
+  if ($(this).val() == "") {
+    $("#submitEmail").prop("disabled", true);
+  } else {
+    $("#submitEmail").prop("disabled", false);
+  }
+});
+var form = document.getElementById("infoForm");
+form.addEventListener("submit", function (event) {
+  if (!form.checkValidity()) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  form.classList.add("was-validated");
+}, false);
